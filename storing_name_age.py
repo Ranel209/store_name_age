e093251e7cbc88856299269cba77a145bf20d4fc
Adrian Ranel <adrianranelm@gmail.com>
2024-10-25 13:01:09 +0800

    added input and dictionary

--- storing_name_age.py
+++ storing_name_age.py
@@ -1,5 +1,21 @@
+store_name_age = {}
+
 #loop to input neccessary information
+while True:
 #auto retry if error occurs
+    while True:
+        try:
+            name = str(input("Enter Your Name: "))
+            age = int(input("Enter Your Age: "))
+            
+            store_name_age [name] = {
+                "age" : age
+            }
 #ask to add more info
+            retry = input("Retry? (y/n): ")
+            break
+        except ValueError:
+            print ("Invalid Input")
+            
 #find oldest age
 #print oldest
